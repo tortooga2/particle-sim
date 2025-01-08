22117c5fd080fdda81e9bf810cf751e8b9de06bf
Chase Nagle <50192480+tortooga2@users.noreply.github.com>
2025-01-08 00:46:24 -0800

    Added some boids and changed the color scheme

--- sketch.js
+++ sketch.js
@@ -604,10 +604,11 @@ function setup() {
     ["vine", "blob", "dot"]
   );
   physicsWorld.addInteraction(
-    applyForce(50, 1.3),
+    applyForce(50, 0.4),
     ["vine", "blob", "dot"],
     ["fish"]
   );
+  physicsWorld.addInteraction(applyForce(200, -0.5), ["fish"], ["vine"]);
 
   physicsWorld.addInteraction(
     applyScreenBounds(width, height, 200, 0.05),
@@ -615,7 +616,7 @@ function setup() {
     []
   );
   //physicsWorld.addInteraction(applyForce(20, 10.3), ["fish"], ["fish"]);
-  physicsWorld.addInteraction(applySeparation(30, 0.05), ["fish"], ["fish"]);
+  physicsWorld.addInteraction(applySeparation(30, 0.01), ["fish"], ["fish"]);
   physicsWorld.addInteraction(applyAlignment(100, 0.05), ["fish"], ["fish"]);
   physicsWorld.addInteraction(applyCohesion(100, 0.0005), ["fish"], ["fish"]);
   physicsWorld.addInteraction(applyMinVelocity(1.0), ["fish"], ["fish"]);
